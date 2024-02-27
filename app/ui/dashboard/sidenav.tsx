705142141c0b32e26e5f667613897c2fcf62dcf1
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-27 17:44:55 -0300

    modify sidenav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -10,7 +10,7 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-indigo-600 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
+        <div className="w-22 md:w-30 text-white">
           <AcmeLogo />
         </div>
       </Link>
